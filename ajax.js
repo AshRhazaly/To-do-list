c9b43d1e26910411f873653ceb110eb757a8d811
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-06 20:21:43 +0800

    Update to do list

--- ajax.js
+++ ajax.js
@@ -6,7 +6,28 @@ function populateUser () {
         var nickname = "";
         nickname += '<option ' +"value=" + key + '>' + value.NickName + '</option>';
         $("#user").append(nickname);
+
       });
+      $.getJSON( "https://to-do-list-cf1af.firebaseio.com/Users/" + $("#user").val() + "/Task.json", function( data ) {
+        var items = [];
+        $("#displayTask").html("");
+        $.each( data, function( key, val ) {
+          items.push( "<li class='list-group-item' id='" + key + "'>" + val.title +"<span class='glyphicon glyphicon-remove delete'>" +"</span>"+ "</li>" );
+          $("#displayTask").append(items);
+        });
+        $(".delete").click(function(){
+          var $delete_list = $(this).parent();
+          $.ajax({
+            url: "https://to-do-list-cf1af.firebaseio.com/Users/" + $("#user").val() + "/Task/" + $(this).parent().attr("id")+ ".json",
+            type: 'DELETE',
+            success: function(data) {
+
+            }
+          })
+          $delete_list.remove();
+        });
+      });
+
     }
   });
 }
@@ -15,10 +36,11 @@ $("#user").change(function(){
   $.ajax({
     url: "https://to-do-list-cf1af.firebaseio.com/Users/" + $("#user").val() + '.json',
     success: function(data) {
+      $("#displayTask").html("");
       $.each(data.Task, function(key, value){
 
         var task_display = "";
-        task_display += "<li>" + value.title + "</li>";
+        task_display += "<li class='list-group-item'> " + value.title +"<span class='glyphicon glyphicon-remove delete'>" +"</span>"+ "</li>";
         $("#displayTask").append(task_display);
       })
     }
@@ -34,14 +56,21 @@ $("#task").keypress(function(e) {
     }
     $.post(
       "https://to-do-list-cf1af.firebaseio.com/Users/" + $("#user").val() + "/Task.json",
-      JSON.stringify(myData)
+      JSON.stringify(myData),
+      function(data) {
+        console.log(data);
+      }
     )
+    // clears input field
     $("#task").val("");
   }
-  // clears input field
 
 
 });
+
+
+
 $(document).ready(function(){
   populateUser();
+
 });
